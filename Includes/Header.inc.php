96c329c9bfe47ae1aefc001add9c0404941d7d3a
Christian Milanes <christianmilanes01@gmail.com>
2024-01-08 06:28:33 +0100

    New components and styles added to the navbar

--- Includes/Header.inc.php
+++ Includes/Header.inc.php
@@ -1,40 +1,104 @@
-<header>
+<header class="navbar navbar-expand-lg navbar-dark bgNav p-3 pe-lg-4 ps-lg-4 pe-md-3 ps-md-3 justify-content-center">
   <!-- Navbar -->
-  <nav class="navbar navbar-expand-md navbar-dark bg-dark bg-gradient p-3 pe-4 ps-4">
+  <nav class="container-xxl">
     <a class="navbar-brand fw-bold ms-2 ms-md-3" href="#">ContentSynth</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
-      <span class="navbar-toggler-icon"></span>
+      <span class="navbar-toggler-icon text-white"></span>
     </button>
     <div class="navbar-collapse collapse" id="menu">
-      <nav class="navbar-nav">
+      <nav class="navbar-nav ms-md-auto">
         <a class="nav-link fw-bold active" href="#">INICIO</a>
-        <a class="nav-link fw-bold" href="#">PELICULAS</a>
-        <a class="nav-link fw-bold" href="#">SERIES</a>
-        <a class="nav-link fw-bold" href="#">LIBROS</a>
-      </nav>
-      <form class="d-flex ms-lg-4" role="search">
-        <div class="input-group">
-          <span class="input-group-text"><i class="bi bi-search"></i></span>
-          <input class="form-control me-1 fw-bold" type="search" placeholder="Buscar...">
+        <!-- Movies tag dropdown menu -->
+        <div class="dropdown">
+          <button class="nav-link fw-bold dropdown-toggle" type="button" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            PELÍCULAS
+          </button>
+          <ul class="dropdown-menu dropdown-menu-dark bgDropdown p-2">
+            <li><a class="dropdown-item rounded-1 mb-1" href="#">Top 30 mejores valoradas</a></li>
+            <li><a class="dropdown-item rounded-1 mb-1" href="#">Experados por los fans</a></li>
+            <li><a class="dropdown-item rounded-1 mb-1" href="#">Explorar por género</a></li>
+            <li><a class="dropdown-item rounded-1 mb-1" href="#">Próximamente</a></li>
+            <li><a class="dropdown-item rounded-1 mb-1" href="#">Noticias</a></li>
+            <li>
+              <hr class="dropdown-divider bg-warning bg-opacity-75">
+            </li>
+            <li><a class="dropdown-item rounded-1 mb-1" href="#">Películas basadas en libros</a></li>
+            <li><a class="dropdown-item rounded-1" href="#">Películas de anime</a></li>
+          </ul>
         </div>
-        <button class="btn btn-warning text-black fw-medium" type="submit">Search</button>
+        <!-- Series tag dropdown menu -->
+        <div class="dropdown">
+          <button class="nav-link fw-bold dropdown-toggle" type="button" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            SERIES
+          </button>
+          <ul class="dropdown-menu dropdown-menu-dark bgDropdown p-2">
+            <li><a class="dropdown-item rounded-1 mb-1" href="#">Top 10 mejores valoradas</a></li>
+            <li><a class="dropdown-item rounded-1 mb-1" href="#">Esperados por los fans</a></li>
+            <li><a class="dropdown-item rounded-1 mb-1" href="#">Explorar por género</a></li>
+            <li><a class="dropdown-item rounded-1 mb-1" href="#">Próximamente</a></li>
+            <li><a class="dropdown-item rounded-1 mb-1" href="#">Noticias</a></li>
+            <li>
+              <hr class="dropdown-divider bg-warning bg-opacity-75">
+            </li>
+            <li><a class="dropdown-item rounded-1 mb-1" href="#">Series basadas en libros</a></li>
+            <li><a class="dropdown-item rounded-1" href="#">Animes</a></li>
+          </ul>
+        </div>
+        <!-- Books tag dropdown menu -->
+        <div class="dropdown">
+          <button class="nav-link fw-bold dropdown-toggle" type="button" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            LIBROS
+          </button>
+          <ul class="dropdown-menu dropdown-menu-dark bgDropdown p-2 mb-2 mb-lg-0">
+            <li><a class="dropdown-item rounded-1 mb-1" href="#">Top 7 mejores valorados</a></li>
+            <li><a class="dropdown-item rounded-1 mb-1" href="#">Experados por los fans</a></li>
+            <li><a class="dropdown-item rounded-1 mb-1" href="#">Explorar por género</a></li>
+            <li><a class="dropdown-item rounded-1" href="#">Más vendidos</a></li>
+            <li>
+              <hr class="dropdown-divider bg-warning bg-opacity-75">
+            </li>
+            <li><a class="dropdown-item" href="#">Novelas ligeras</a></li>
+          </ul>
+        </div>
+      </nav>
+      <!-- Search content form -->
+      <form class="d-flex ms-lg-4 me-md-auto" role="search">
+        <div class="input-group">
+          <span class="input-group-text"><i class="bi bi-search textGradient"></i></span>
+          <input class="form-control fw-bold" type="search" placeholder="Buscar...">
+        </div>
       </form>
-      <nav class="navbar-nav ms-auto me-2 me-md-3">
-        <a class="nav-link ms-4 fw-bold" href="#">INICIAR SESIÓN</a>
+      <nav class="navbar-nav me-2 me-lg-3 me-md-1">
+        <a class="nav-link ms-lg-4 me-lg-4 fw-bold" href="#">INICIAR SESIÓN</a>
         <!-- Dropdown menu -->
         <div class="dropdown dropstart ps-2 profileDd">
-          <button class="btn p-0 profileBtn" type="button"
-            data-bs-toggle="dropdown" aria-expanded="false">
-            <img class="img-fluid p-0" style="border-radius: 50%;max-width: 37px;" src="" alt="Tu foto de perfil">
+          <button class="btn p-0 profileBtn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <img class="img-fluid p-0 profileImg" style="border-radius: 50%;max-width: 37px;" src=""
+              alt="Tu foto de perfil">
           </button>
-          <ul class="dropdown-menu dropdown-menu-dark">
-            <li><a class="dropdown-item active" href="#">Mis listas</a></li>
-            <li><a class="dropdown-item" href="#">Establecer preferecias</a></li>
-            <li><a class="dropdown-item" href="#">Cambiar foto de perfil</a></li>
+          <ul class="dropdown-menu dropdown-menu-dark bgDropdown">
+            <li><a class="dropdown-item rounded-1 mb-1" href="#">Favoritos</a></li>
+            <li><a class="dropdown-item rounded-1 mb-1" href="#">Mis listas</a></li>
+            <li><a class="dropdown-item rounded-1 mb-1" href="#">Preferecias</a></li>
+            <li><a class="dropdown-item rounded-1" href="#">Recomendaciones</a></li>
             <li>
               <hr class="dropdown-divider">
             </li>
             <li><a class="dropdown-item" href="#">Cerrar Sesión</a></li>
+          </ul>
+        </div>
+        <!-- Language dropdown button -->
+        <div class="dropdown">
+          <button class="nav-link fw-bold dropdown-toggle active" type="button" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            ES
+          </button>
+          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-dark bgDropdown p-2">
+            <li><a class="dropdown-item rounded-1 mb-1 active" href="#">Español (ES)</a></li>
+            <li><a class="dropdown-item rounded-1" href="#">English (EN)</a></li>
           </ul>
         </div>
       </nav>
